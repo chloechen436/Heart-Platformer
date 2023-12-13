edf2403d77200ccf7599531b9112bbbe83ddbdc8
Jtotheazzy <jahuang7@s.sfusd.edu>
2023-12-13 13:28:58 -0800

    almost finished pt1

--- Heart Platformer/player.gd
+++ Heart Platformer/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const ACCELERATION = 400.0
+const ACCELERATION = 600.0
+const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -14,8 +15,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+			velocity.y = JUMP_VELOCITY / 2
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,6 +28,6 @@ func _physics_process(delta):
 	if direction != 0:
 		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
 	else:
-		velocity.x = 0
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
 	move_and_slide()
